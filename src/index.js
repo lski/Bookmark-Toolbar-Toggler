1ef25a5bb175925cde75db79f521c5f2864d27c6
Lee Cooper <lee.cooper@lski.uk>
2015-06-14 18:44:18 +0100

    Added options for the user to change key bindings

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 "use strict;";
 
 const BOOKMARK_TOOLBAR_SELECTOR = "#PersonalToolbar";
+const HOTKEY_ENABLED_OPTION = 'hotkeyEnabled';
+const HOTKEY_OPTION = "hotkeys";
 
 const buttons = require('sdk/ui/button/action');
 const utils = require('sdk/window/utils');
 const hotkeys = require("sdk/hotkeys");
+const options = require('sdk/simple-prefs');
 
-var button = createButton();
-var hotkey = createHotkey();
-
-function handler(state) {
-	toggleBookmarkToolbars();
-}
+var hotkey = initHotkey(),
+    button = initButton();
 
 function toggleBookmarkToolbars() {
-    
+
     let toolbars = getBookmarkToolbars(getWindows());
-    
-    if(!toolbars.length) {
+
+    if (!toolbars.length) {
         return;
     }
-    
+
     let visible = null;
 
-    for(let toolbar of toolbars){
-        
-        if(visible === null) {
+    for (let toolbar of toolbars) {
+
+        if (visible === null) {
             visible = !!toolbar.collapsed;
         }
-        
+
         toolbar.ownerGlobal.setToolbarVisibility(toolbar, visible);
     }
 }
@@ -39,31 +38,60 @@ function getWindows() {
 
 function getBookmarkToolbars(windows) {
 
-    let toolbars = windows.map((w) => { 
-        return w.document.querySelector(BOOKMARK_TOOLBAR_SELECTOR); 
+    let toolbars = windows.map((w) => {
+        return w.document.querySelector(BOOKMARK_TOOLBAR_SELECTOR);
     });
-    
-	return toolbars;
+
+    return toolbars;
 }
 
-function createHotkey() {
-    
-    hotkeys.Hotkey({
-        combo: "control-alt-b",
-        onPress: handler
-    })
+function enableHotkey() {
+
+    return hotkeys.Hotkey({
+            combo: options.prefs[HOTKEY_OPTION],
+            onPress: toggleHandler
+        });
 }
 
-function createButton(){
-    
+function disableHotkey(hotkey) {
+
+    if (hotkey != null) {
+        // small workaround for a bug with the sandbox not being able to travel up the proto chain
+        hotkey.__proto__.destroy.call(hotkey);
+    }
+
+    return null;
+}
+
+function initButton() {
+
     return buttons.ActionButton({
-      id: "btn-show",
-      label: "Show/Hide Bookmarks Toolbar",
-      icon: {
-        "16": "./icon-16.png",
-        "32": "./icon-32.png",
-        "64": "./icon-64.png"
-      },
-      onClick: handler
+        id: "btn-show",
+        label: "Show/Hide Bookmarks Toolbar",
+        icon: {
+            "16": "./icon-16.png",
+            "32": "./icon-32.png",
+            "64": "./icon-64.png"
+        },
+        onClick: toggleHandler
     });
+}
+
+function initHotkey() {
+
+    // Handle both the option changes the same as both require new hotkey objects
+    options.on("", hotkeyOptionsChangedHandler);
+
+    if (options.prefs[HOTKEY_ENABLED_OPTION]) {
+        return enableHotkey();
+    }
+}
+
+function toggleHandler(state) {
+    toggleBookmarkToolbars();
+}
+
+function hotkeyOptionsChangedHandler() {
+    // look at the fact we are not destorying the old hot key
+    hotkey = options.prefs[HOTKEY_ENABLED_OPTION] ? enableHotkey() : disableHotkey(hotkey);
 }
